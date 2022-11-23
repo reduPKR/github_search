e46c1ef0831c448553cb75436045268393fc9232
Rafael Eduardo <56879793+reduPKR@users.noreply.github.com>
2022-11-23 16:53:53 -0300

    comunicação webhook

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
 import requests
 import json
 
-url = "https://api.github.com/search/code?q={}-user:reduPKR&in:file&sort=created&order=desc"
-token = ""
+def get_data(item):
+    data = {
+        "proprietário": item['repository']['owner']['login'],
+        "repositório": item['repository']['name'],
+        "repositório completo": item['repository']['full_name'],
+        "url do código": item['html_url'],
+        "nome do arquivo": item['name'],
+        "privado": item['repository']['private'],
+        "descrição": item['repository']['description']
+    }
 
-headers = {
-    'authorization': 'Bearer {}'.format(token)
-}
+    return json.dumps(data)
 
-with open("lista.txt") as file:
-    for line in file:
-        url = url.format(line)
-        response = requests.request("GET", url, headers=headers)
-        response_json = response.json()
-        #print(response_json["items"])
 
-        for item in response_json["items"]:
-            print(
-                f"proprietário: {item['repository']['owner']['login']}\n"
-                f"repositório : {item['repository']['name']}\n"
-                f"repositório completo: {item['repository']['full_name']}\n"
-                f"privado: {item['repository']['private']}\n"
-                f"nome do arquivo: {item['name']}\n"
-                f"url do código: {item['html_url']}"
-            )
-            print("-"*80)
+def send_to_webhook(item):
+    print(
+        f"proprietário: {item['repository']['owner']['login']}\n"
+        f"repositório : {item['repository']['name']}\n"
+        f"repositório completo: {item['repository']['full_name']}\n"
+        f"url do código: {item['html_url']}\n"
+        f"nome do arquivo: {item['name']}\n"
+        f"privado: {item['repository']['private']}\n"
+        f"descrição: {item['repository']['description']}"
+    )
+    print("-" * 80)
+
+    webhook_url = "https://webhook.site/9cbac4c0-92bb-4647-aebe-c0c223e35c0e"
+    header = {
+        'Content-Type': 'application/json'
+    }
+
+    requests.post(webhook_url, data=get_data(item), headers=header)
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/search/code?q={}-user:MMMMM&in:file&sort=created&order=desc"
+    token = "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"
+
+    header = {
+        'authorization': 'Bearer {}'.format(token)
+    }
+
+    with open("lista.txt") as file:
+        for line in file:
+            url = url.format(line)
+            response = requests.request("GET", url, headers=header)
+            response_json = response.json()
+            #print(response_json["items"])
+
+            for item in response_json["items"]:
+                send_to_webhook(item)
